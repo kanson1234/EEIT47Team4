7194f6b6344c18853ad37f0de36c1ed88788a171
Kevin <kevin.lin0525ls@gmail.com>
2022-08-06 18:34:53 +0800

    Testing

---
+++ springbootdemo_teacher/src/main/java/com/ispan/springboot/model/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.ispan.springboot.model;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long>{
+	
+	
+}
+
