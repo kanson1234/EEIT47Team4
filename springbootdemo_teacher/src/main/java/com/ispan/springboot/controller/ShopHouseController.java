7fe77aa305b5f79f2ec642a0b1791d9f13a379d7
Roy <roywangpt@gmail.com>
2022-08-06 13:25:56 +0800

    更新

--- springbootdemo_teacher/src/main/java/com/ispan/springboot/controller/ShopHouseController.java
+++ springbootdemo_teacher/src/main/java/com/ispan/springboot/controller/ShopHouseController.java
@@ -1,5 +1,11 @@
 package com.ispan.springboot.controller;
 
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+
 public class ShopHouseController {
 
+	
 }
--- springbootdemo_teacher/src/main/java/com/ispan/springboot/service/ShopHouseService.java
+++ springbootdemo_teacher/src/main/java/com/ispan/springboot/service/ShopHouseService.java
@@ -1,6 +1,42 @@
 package com.ispan.springboot.service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ispan.springboot.dao.ShopHouseDao;
+import com.ispan.springboot.model.ShopHouseBean;
+
+@Service
+@Transactional
 public class ShopHouseService {
+	
+	@Autowired
+	private ShopHouseDao sDao;
+	
+	//新增商品
+	public void insertItem(ShopHouseBean shb) {
+		sDao.save(shb);
+	}
+	
+	//刪除商品
+	public void deleteItem(Integer id) {
+		sDao.deleteById(id);
+	}
+	
+	//修改商品資訊
+	
+	
+	//查詢一筆商品
+	public shop findItemById(Integer id) {
+		sDao.findById(id);
+	}
+	
+	
+	
+	
+	
 	
 
 	
