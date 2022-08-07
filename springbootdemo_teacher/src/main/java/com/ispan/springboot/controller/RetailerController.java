055bb8e02dd6e30bb23f3fee5fcf6fc12a53abe3
Kevin <kevin.lin0525ls@gmail.com>
2022-08-07 19:30:36 +0800

    testing

--- springbootdemo_teacher/src/main/java/com/ispan/springboot/controller/RetailerController.java
+++ springbootdemo_teacher/src/main/java/com/ispan/springboot/controller/RetailerController.java
@@ -1,8 +1,10 @@
 package com.ispan.springboot.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +20,13 @@ public class RetailerController {
 	@Autowired
 	private RetailerRepository rDao;
 	
+	@GetMapping("Retailer/RetailerCRUD/{Id}")
+	public String showAllCustomer(@PathVariable("Id") Integer id,Model model ) {
+		List<RetailerBean> list = rDao.findAll(); 
+		model.addAttribute("id",);
+		return "RetailerCRUD";
+	}
+	
 	@PostMapping("Retailer/insert")
 	public RetailerBean insert() {
 		RetailerBean r1 = new RetailerBean();
@@ -32,8 +41,14 @@ public class RetailerController {
 		return respRetailer;
 		
 	}
-	@GetMapping("Retailer/get")
-	public RetailerBean getCustomerById(@RequestParam Integer id) {
+	
+	@GetMapping("customer/findByName2")
+	public List<RetailerBean> findByName2(@RequestParam String firstName){
+		return rDao.findCustomerByName2(firstName);	
+		}
+	
+	@GetMapping("Retailer/get/{id}")
+	public RetailerBean getCustomerById(@PathVariable Integer id) {
 		Optional<RetailerBean> optional = rDao.findById(id);
 		if(optional.isPresent()) {
 			return optional.get();
--- springbootdemo_teacher/src/main/java/com/ispan/springboot/model/RetailerRepository.java
+++ springbootdemo_teacher/src/main/java/com/ispan/springboot/model/RetailerRepository.java
@@ -1,9 +1,14 @@
 package com.ispan.springboot.model;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RetailerRepository extends JpaRepository<RetailerBean, Integer>{
-
+	@Query(value="select * from Retailer where C2_FirstName = :name", nativeQuery = true)
+	public List<RetailerBean> findCustomerByName2(@Param("name") String firstName);
 }
