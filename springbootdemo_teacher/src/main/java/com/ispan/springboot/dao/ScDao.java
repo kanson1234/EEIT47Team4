06c5b86bb69746fc68b50cbece4a746b9e529dc9
Alex5025 <tsai5025tunghan@gmail.com>
2022-08-22 13:56:02 +0800

    TRY

--- springbootdemo_teacher/src/main/java/com/ispan/springboot/dao/ScDao.java
+++ springbootdemo_teacher/src/main/java/com/ispan/springboot/dao/ScDao.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ispan.springboot.model.ShoopingCar;
 
