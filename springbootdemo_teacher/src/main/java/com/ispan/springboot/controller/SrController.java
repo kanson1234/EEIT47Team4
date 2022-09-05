6e62fba07d34457d74e795e8a483282fc6b873d5
Alex5025 <tsai5025tunghan@gmail.com>
2022-09-05 22:38:43 +0800

    整理

--- springbootdemo_teacher/src/main/java/com/ispan/springboot/controller/SrController.java
+++ springbootdemo_teacher/src/main/java/com/ispan/springboot/controller/SrController.java
@@ -4,10 +4,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- springbootdemo_teacher/src/main/java/com/ispan/springboot/model/ShoppingRecord.java
+++ springbootdemo_teacher/src/main/java/com/ispan/springboot/model/ShoppingRecord.java
@@ -1,6 +1,5 @@
 package com.ispan.springboot.model;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -11,7 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
